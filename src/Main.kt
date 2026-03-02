b28a45732a0dd03a79481d55fa91c981c67f34b1
Xandetds <alexandretibes9@gmail.com>
2026-03-02 20:49:24 -0300

    feat: Adiciona arquivo final do exercício validador

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,21 @@
+
 fun main() {
+    val carros = listOf("Cerato", "Corolla", "Golf", "Fusca", "Opala")
+
+
     val listaDeRegras = listOf(
         Requisito("Mínimo de 5 caracteres") { it.length >= 5 },
-        Requisito("Deve conter o ano do Hexa (2026)") { it.contains("2026") },
-        Requisito("Deve conter uma letra maiúscula") {it.contains(StringBuilder)},
-        Requisito("Deve conter menos de 30 caracteres") {it.length < 30}
+
+        Requisito("Deve conter um número menor que 5") {it.any{numero -> numero.isDigit() && numero.digitToInt() < 5}},
+
+        Requisito("Deve conter uma letra maiúscula") {it.any{letra -> letra.isUpperCase()}},
+
+        Requisito("Deve conter um carro") {carros.any{carro -> it.contains(carro)}},
+
+        Requisito("Deve conter um caracter especial") {it.any{caracter
+            -> !caracter.isDigit() && !caracter.isLetter() && !caracter.isWhitespace()}},
+
+        Requisito("Deve conter 17 caracteres ou menos") {it.length <= 17}
     )
 
     var senhaAprovada = false
@@ -21,15 +33,15 @@ fun main() {
             }
         }
         if (erroEncontrado != null) {
-            println("❌ ERRO: $erroEncontrado")
+            println("ERRO: $erroEncontrado")
         } else {
-            println("✅ SUCESSO! Senha aceite pelo Overlord.")
+            println("SUCESSO! Senha aceite pelo Overlord.")
             senhaAprovada = true
         }
     } while (!senhaAprovada)
 }
 
-class Requisito(
-
+data class Requisito(
+    val mensagemErro: String,
+    val validacao: (String) -> Boolean
 )
-
